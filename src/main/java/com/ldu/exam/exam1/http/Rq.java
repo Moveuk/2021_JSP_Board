640c7e1fb048e40ceb5d3af50bf4632226b94bc7
moveuk <v_donguk@naver.com>
2021-08-01 23:35:43 +0900

    Req와 Resp의 어탭터 클래스인 Rq 클래스 구현

---
+++ src/main/java/com/ldu/exam/exam1/http/Rq.java
@@ -0,0 +1,71 @@
+package com.ldu.exam.exam1.http;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.Getter;
+
+@Getter
+public class Rq{
+	private HttpServletRequest request;
+	private HttpServletResponse response;
+	private boolean isInvalid = false;
+	private String controllerTypeName;
+	private String controllerName;
+	private String actionMethodName;
+	
+	public Rq(HttpServletRequest request, HttpServletResponse response) {
+		// 들어오는 파라미터를 UTF-8로 해석
+		try {
+			request.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		// 서블릿이 html 만들때 utf - 8로 만듬.
+		response.setCharacterEncoding("UTF-8");	
+		
+		// 브라우저에 html 파일이 uft-8 형식이라고 알려줌.
+		response.setContentType("text/html; charset=UTF-8");
+		
+		this.request = request;
+		this.response = response;
+		
+		// /2021_jsp_board/usr/article/wefwe
+				// 들어오는 주소값 받아서 /기준으로 스플릿하고 5 보다 작으면 잘못된 주소 리턴
+				// [] / [2021_jsp_board] / [usr] / [article] / [wefwe]
+				String requestUri = request.getRequestURI();
+				String[] requestUriBits = requestUri.split("/");
+
+				int minBitsCount = 5;
+				
+				if(requestUriBits.length < minBitsCount) {
+					isInvalid = true;
+					return;
+				}
+				
+				// /usr/article/write 이 세 개를 꺼내옴
+				int controllerTypeNameIndex = 2;
+				int controllerNameIndex = 3;
+				int actionMethodNameIndex = 4;
+				
+				this.controllerTypeName = requestUriBits[controllerTypeNameIndex];
+				this.controllerName = requestUriBits[controllerNameIndex];
+				this.actionMethodName = requestUriBits[actionMethodNameIndex];
+	}
+
+	public void print(String string) {
+		try {
+			response.getWriter().append(string);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void println(String string) {
+		print(string + "\n");
+	}
+
+}
--- src/main/java/com/ldu/exam/exam1/servlet/DispatcherServlet.java
+++ src/main/java/com/ldu/exam/exam1/servlet/DispatcherServlet.java
@@ -8,58 +8,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ldu.exam.exam1.http.Rq;
 import com.ldu.mysqlutil.MysqlUtil;
 
 @WebServlet(urlPatterns = { "/usr/*" })
 public class DispatcherServlet extends HttpServlet {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		// 들어오는 파라미터를 UTF-8로 해석
-		request.setCharacterEncoding("UTF-8");
-		
-		// 서블릿이 html 만들때 utf - 8로 만듬.
-		response.setCharacterEncoding("UTF-8");	
-		
-		// 브라우저에 html 파일이 uft-8 형식이라고 알려줌.
-		response.setContentType("text/html; charset=UTF-8");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
 
 		// multiple url bind
-		response.getWriter().append("멀티플 바인드 성공");
+//		response.getWriter().append("멀티플 바인드 성공");
 		
-		// /2021_jsp_board/usr/article/wefwe
-		// 들어오는 주소값 받아서 /기준으로 스플릿하고 5 보다 작으면 잘못된 주소 리턴
-		// [] / [2021_jsp_board] / [usr] / [article] / [wefwe]
-		String requestUri = request.getRequestURI();
-		String[] requestUriBits = requestUri.split("/");
-
-		int minBitsCount = 5;
+		Rq rq = new Rq(request, response);
 		
-		if(requestUriBits.length < minBitsCount) {
-			response.getWriter().append("올바른 요청이 아닙니다.");
-			return;
+		if (rq.isInvalid()) {
+			rq.print("올바른 요청이 아닙니다.");
 		}
 		
-		// /usr/article/write 이 세 개를 꺼내옴
-		int controllerTypeNameIndex = 2;
-		int controllerNameIndex = 3;
-		int actionMethodNameIndex = 4;
-		
-		String controllerTypeName = requestUriBits[controllerTypeNameIndex];
-		String controllerName = requestUriBits[controllerNameIndex];
-		String actionMethodName = requestUriBits[actionMethodNameIndex];
-		
 		// usrArticleController write 가 호출되면 됨.
-		response.getWriter().append("<br>");
-		response.getWriter().append("controllerTypeName : " + controllerTypeName);
-		response.getWriter().append("<br>");
-		response.getWriter().append("controllerName : " + controllerName);
-		response.getWriter().append("<br>");
-		response.getWriter().append("actionMethodName : " + actionMethodName);
+		rq.println("controllerTypeName : " + rq.getControllerTypeName());
+		rq.println("<br>");
+		rq.println("controllerName : " + rq.getControllerName());
+		rq.println("<br>");
+		rq.println("actionMethodName : " + rq.getActionMethodName());
 		
 		// db 커넥션
 		MysqlUtil.setDBInfo("localhost", "moveuk", "1234", "jsp_board");
-
 		MysqlUtil.setDevMode(true);
 
 		// MysqlUtil에서 제대로 못닫았으면 닫아주는 용도
